d73eab7175084aaaaa678eee354b951b25854af6
Parth Rana <77335035+8bit-Parth@users.noreply.github.com>
2023-08-11 19:17:23 +0530

    Time Update

--- js/script.js
+++ js/script.js
@@ -39,7 +39,7 @@ var swiper = new Swiper(".home-slider", {
     loop:true,
 });
 
-let countDate = new Date('August 12, 2023 00:00:00').getTime();
+let countDate = new Date('August 15, 2023 00:00:00').getTime();
 
 function countDown(){
 
